231d2cac3f0209964e0b7687a01d2c7ab1e04408
Selçuk Talha Kul <157029095+selcuktk@users.noreply.github.com>
2025-07-09 16:34:44 +0300

    feat: response update

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,13 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
-
+from components.SelcukGray.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, SelcukGrayOutputs, SelcukGrayResponse, SelcukGrayExecutor, OutputImage
 
 def build_response(context):
     outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
+    Outputs = SelcukGrayOutputs(outputImage=outputImage)
+    selcukGrayResponse = SelcukGrayResponse(outputs=Outputs)
+    selcukGrayExecutor = SelcukGrayExecutor(value=selcukGrayResponse)
+    executor = ConfigExecutor(value=selcukGrayExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
